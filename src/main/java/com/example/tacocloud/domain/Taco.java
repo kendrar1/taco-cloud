ecc7aff2e2797b2cf38390a50ff4f24c4972a79f
Kendra Reynolds <krey214@wgu.edu>
2023-04-02 13:35:48 -0700

    Added page for selecting taco ingredients

---
+++ src/main/java/com/example/tacocloud/domain/Taco.java
@@ -0,0 +1,10 @@
+package com.example.tacocloud.domain;
+import java.util.List;
+import lombok.Data;
+
+
+@Data
+public class Taco {
+    private String name;
+    private List<Ingredient> ingredients;
+}
